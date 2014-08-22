f6edd29aa10c385a049457996ed7b24a9b88e162
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-08-21 23:22:10 -0300

    create tasks in controller, specs

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('demoApp.controllers', [])
-  .controller('TasksController', ['$scope', function($scope) {
+angular.module('demoApp.controllers', []).controller('TasksController', ['$scope', function($scope) {
+  $scope.tasks = []
 
-  }]);
+  $scope.init = function() {
+    $scope.createTask('Call Charlie for his birthday');
+    $scope.createTask('Invite friends to come over for the game');
+    $scope.createTask('Do groceries');
+  }
+
+  $scope.createTask = function(description) {
+    var task = {
+      description: description,
+      status: 'active'
+    }
+    $scope.tasks.push(task);
+  }
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,5 +1,32 @@
 'use strict';
 
-describe('controllers', function(){
+describe('TasksController', function(){
   beforeEach(module('demoApp.controllers'));
+
+  it('should have a list of tasks', inject(function($rootScope, $controller) {
+    var scope = $rootScope.$new();
+    var controller = $controller('TasksController', { $scope: scope });
+
+    expect(scope.tasks).toBeDefined();
+  }));
+
+  it("should be able to create tasks with description and default _active_ status", inject(function($rootScope, $controller) {
+    var scope = $rootScope.$new();
+    var controller = $controller('TasksController', { $scope: scope });
+
+    expect(scope.tasks.length).toBe(0);
+    scope.createTask('some tasks');
+    expect(scope.tasks.length).toBe(1);
+    expect(scope.tasks[0].description).toBe('some tasks');
+    expect(scope.tasks[0].status).toBe('active');
+  }));
+
+  it("should initialize with 3 tasks", inject(function($rootScope, $controller) {
+    var scope = $rootScope.$new();
+    var controller = $controller('TasksController', { $scope: scope });
+
+    expect(scope.tasks.length).toBe(0);
+    scope.init();
+    expect(scope.tasks.length).toBe(3);
+  }));
 });
