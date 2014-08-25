13c8fa98cece3231a8d358b014d56e9debfad8a8
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-08-25 19:38:37 -0300

    wire add new tasks into view, add e2e scenarios

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -54,11 +54,47 @@ describe("Home page", function() {
     });
   });
 
+  describe(", while clicking New task button", function() {
+    it("should show input to enter new task", function() {
+      browser.get('#/home');
+      expect(element.all(by.model('newTask.description')).count()).toBe(0);
+      clickNewTask();
+      expect(element.all(by.model('newTask.description')).count()).toBe(1);
+    });
+
+    it('should change its text', function(){
+      browser.get('#/home');
+      expect(element(by.css('.btn.btn-xs.btn-info')).getText()).toEqual('Add new task');
+      clickNewTask();
+      expect(element(by.css('.btn.btn-xs.btn-info')).getText()).toEqual('Close');
+    });
+  });  
+
+  describe(", while clicking Add task button", function() {
+    it('should display task in the list', function(){
+      browser.get('#/home');
+      clickNewTask();
+      element(by.model('newTask.description')).sendKeys('my new task');
+      clickAddTask();
+      element.all(by.binding('task.description')).last().getText().then(function(text) {
+        expect(text).toEqual('my new task');
+      });
+    });
+  });  
+
   function clickFirstDoneTask() {
     element.all(by.css('.btn.btn-primary.btn-xs')).first().click();
   }
 
   function clickShowCompleted() {
     element(by.css('.btn.btn-default.btn-xs')).click();
+  }
+
+  function clickNewTask() {
+    element(by.css('.btn.btn-xs.btn-info')).click();
+  }
+
+  function clickAddTask() {
+    element(by.css('.btn.btn-success')).click();
   }
 });
